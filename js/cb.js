396b5a0aab0e02d878bead9a4d3f5903d6011592
smmersch26 <smmersch26@gmail.com>
2016-12-22 21:12:38 -0500

    post-class commit

--- js/cb.js
+++ js/cb.js
@@ -1,3 +1,17 @@
 $("i").on("click", function() {
   $("nav").toggleClass("open");
 });
+
+$(window).resize(function(){
+	if($(window).width() < 767){
+		$(".nav-option span").on("click",function(){
+			$(this).siblings().slideToggle();
+		})
+	}
+});
+
+if($(window).width() < 767){
+	$(".nav-option span").on("click",function(){
+		$(this).siblings().slideToggle();
+	});
+};
