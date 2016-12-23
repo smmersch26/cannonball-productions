0ec7373f5d9c8e3c9b3b3e69039a1672cb73804d
smmersch26 <smmersch26@gmail.com>
2016-12-23 08:52:26 -0500

    window width

--- js/cb.js
+++ js/cb.js
@@ -2,13 +2,13 @@ $("i").on("click", function() {
   $("nav").toggleClass("open");
 });
 
-$(window).resize(function(){
-	if($(window).width() < 767){
-		$(".nav-option span").on("click",function(){
-			$(this).siblings().slideToggle();
-		})
-	}
-});
+// $(window).resize(function(){
+// 	if($(window).width() < 767){
+// 		$(".nav-option span").on("click",function(){
+// 			$(this).siblings().slideToggle();
+// 		})
+// 	}
+// });
 
 if($(window).width() < 767){
 	$(".nav-option span").on("click",function(){
